31747beca350adbde1cd5c0ea8cbcf307e4eaba0
Aaron Saenger <theredbaron22@gmail.com>
2025-05-06 21:41:24 -0400

    .

--- beforeScript.sh
+++ beforeScript.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/usr/bin/bash
 
 . /home/pi/wittypi/utilities.sh
 
@@ -7,6 +7,9 @@ alarmStart=$(hex2dec $alarmStart)
 
 alarmShut=$(/usr/sbin/i2cget -y 1 0x08 10)
 alarmShut=$(hex2dec $alarmShut)
+
+lastAction=$(/usr/sbin/i2cget -y 1 0x08 11)
+lastAction=$(hex2dec $lastAction)
 
 lowV=$(/usr/sbin/i2cget -y 1 0x08 8)
 lowV=$(hex2dec $lowV)
@@ -17,21 +20,22 @@ if [[ $alarmStart -eq 1 ]]; then
     /usr/bin/python3 /home/pi/Pollinator_Camera/Create_Schedule.py
     /usr/bin/echo "Scheduale.wpi created"
     /home/pi/wittypi/runScript.sh
-    /usr/bin/cat /home/pi/wittypi/schedule.wpi
+    #/usr/bin/cat /home/pi/wittypi/schedule.wpi
 fi
 
 if [[ $lowV -eq 1 ]]; then
     /usr/bin/echo "wakeup after low voltage shutdown and subsequent recharge period"
-    /usr/bin/cat /home/pi/wittypi/schedule.wpi
+    #/usr/bin/cat /home/pi/wittypi/schedule.wpi
     /usr/bin/python3 /home/pi/Pollinator_Camera/Create_Schedule.py
-    /usr/bin/cat /home/pi/wittypi/schedule.wpi
+    #/usr/bin/cat /home/pi/wittypi/schedule.wpi
     /home/pi/wittypi/runScript.sh
     /usr/bin/tail -n 5 /home/pi/wittypi/schedule.log
-
+fi
 #if these flags are all down, then it probably means the camera was started manually
 # this could be from the button or auto on by plugging in the usb
-if [ $lowV -eq 0 ] && [ $alarmShut -eq 0 ] && [ $alarmStart -eq 0 ]; then
-    /usr/bin/echo "low voltage and both wake and shutdown flags are down.\nAssuming this is the first time the Camera has been deployed"
-    /usr/bin/python3 /home/pi/Pollinator_Camera?Create_Schedule.py 
-    /home/pi/wittypi/runScript.sh
+if [ $lastAction -eq 3 ]; then 
+    /usr/bin/echo $'button clicked to start. \n runScript.sh should automatically revise time to next schedule to compnesate"'
+
+    #/usr/bin/python3 /home/pi/Pollinator_Camera/Create_Schedule.py 
+    #/home/pi/wittypi/runScript.sh
 fi
